9e3628bf912b89d8ed495069235170fa9097fbf2
Darian Tavana <datavana@gmail.com>
2019-02-18 16:34:29 +0000

    added error handling

---
+++ modules/errors.py
@@ -0,0 +1,33 @@
+import discord
+import asyncio
+from discord.ext import commands
+import traceback
+import sys
+
+#Misc. Modules
+import datetime
+import config as cfg
+
+
+class Errors:
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def on_command_error(self, ctx, error):
+        if hasattr(ctx.command, 'on_error'):
+            return
+        ignored = (commands.CommandNotFound, commands.CheckFailure)
+        error = getattr(error, 'original', error)
+        if (isinstance(error, ignored)):
+            return 
+        elif (isinstance(error, commands.MissingRequiredArgument)):
+            return await self.bot.sendError(ctx, f"`{error.param.name}` is a missing argument that is required!")
+        elif (isinstance(error, commands.BadArgument)):
+            return await self.bot.sendError(ctx, f"Invalid argument: `{error}`")
+        elif (isinstance(error, commands.DisabledCommand)):
+            return await self.bot.sendError(ctx, f'{ctx.command} has been disabled.')
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+def setup(bot):
+    bot.add_cog(Errors(bot))
